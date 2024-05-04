6c9079bf205316a7f8dac3eab3269bb7ea846ab7
AliAlSubhi98 <alialsubhi1998@gmail.com>
2024-05-04 21:58:54 +0400

    fmt to print

---
+++ 00/main.go
@@ -0,0 +1,23 @@
+package main
+
+/* 				ctrl + s
+to save & import all the missing imports */
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello There ❤️")
+	fmt.Println(`
+	Poem of the Day
+	Groovin’ Low
+	BY A. B. SPELLMAN
+	my swing is more mellow
+	these days: not the hardbop drive
+	i used to roll but more of a cool
+	foxtrot. my eyes still close
+	when the rhythm locks; i’ve learned
+	to boogie with my feet on the floor
+	i’m still movin’, still groovin’
+	still fallin’ in...
+	`)
+}
